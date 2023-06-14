b981e2f72090eecb8ba1d41f7563d241df6cb244
Victor Hernandez <vichdz97@txstate.edu>
2023-06-14 14:26:35 -0500

    added test cases

--- backend/src/test/java/com/example/authsys/AuthenticationSystemApplicationTests.java
+++ backend/src/test/java/com/example/authsys/AuthenticationSystemApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.authsys;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.backend.User;
+
 @SpringBootTest
+@SpringBootConfiguration
 class AuthenticationSystemApplicationTests {
 
+	User user = new User(0, "user2023", "User!2023", "User");
+
 	@Test
-	void contextLoads() {
+	void correctUsername() {
+		assertEquals("user2023", user.getUsername());
 	}
 
+	@Test
+	void correctPassword() {
+		assertEquals("User!2023", user.getPassword());
+	}
+	
+	@Test
+	void correctRole() {
+		assertEquals("User", user.getRole());
+	}
 }
