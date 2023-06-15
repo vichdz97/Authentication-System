47960cfe49071e83b0f53cc44d732791e1fa4b75
Victor Hernandez <vichdz97@txstate.edu>
2023-06-15 16:47:50 -0500

    redesigned login page

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   users!: User[];
   userID: number = 0;
-  alertMessage: string = '';
+  invalidMsg: string = '';
 
   loginForm = this.fb.group({
     username: ['', [Validators.required]],
@@ -51,7 +51,7 @@ export class LoginComponent implements OnInit {
         this.redirectUser();
       }
       else {
-        this.alertMessage = 'Invalid login attempt.';
+        this.invalidMsg = 'Invalid login attempt.';
         this.loginForm.reset();
       }
     }
