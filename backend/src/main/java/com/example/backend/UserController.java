bdb5b47a54bcda0163a85e97654dd045ec16cacc
Victor Hernandez <vichdz97@txstate.edu>
2023-06-15 04:43:43 -0500

    enabled CORS

--- backend/src/main/java/com/example/backend/UserController.java
+++ backend/src/main/java/com/example/backend/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@CrossOrigin(origins = "http://authentication-system.s3-website.us-east-2.amazonaws.com")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
