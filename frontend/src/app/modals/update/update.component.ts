47d1a33693613a1226e2b15060b1245626b57e27
Victor Hernandez <vichdz97@txstate.edu>
2023-06-19 21:02:23 -0500

    added html and update logic to update component

--- frontend/src/app/modals/update/update.component.ts
+++ frontend/src/app/modals/update/update.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-update',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateComponent implements OnInit {
 
-  constructor() { }
+    @Input() userID!: number;
+    user?: User;
+    updatedName?: string;
+    updatedPassword?: string;
+    updatedRole?: string;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        public activeModal: NgbActiveModal,
+        private userService: UserService
+    ) { }
 
+    ngOnInit(): void {
+        this.userService.getUserToModify(this.userID).subscribe({
+            next: user => this.user = user,
+            error: err => console.error("ERROR - Could not retrieve user"),
+            complete: () => console.log("SUCCESS - User retrieved")
+        });
+    }
+
+    updateUser() {
+        let updatedUser: User = <User> {
+            ...this.user,
+            username: this.updatedName ? this.updatedName : this.user?.username,
+            password: this.updatedPassword ? this.updatedPassword : this.user?.password,
+            role: this.updatedRole ? this.updatedRole : this.user?.role
+        };
+        this.activeModal.close(updatedUser);
+    }
 }
