15056a5ee2949129170f4c708bc214ccb49fb7b6
Victor Hernandez <vichdz97@txstate.edu>
2023-06-20 01:33:32 -0500

    converted inputs to form controls and added existing account logic

--- frontend/src/app/modals/update/update.component.ts
+++ frontend/src/app/modals/update/update.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { User } from 'src/app/interfaces/user';
 import { UserService } from 'src/app/services/user.service';
@@ -11,17 +12,28 @@ import { UserService } from 'src/app/services/user.service';
 export class UpdateComponent implements OnInit {
 
     @Input() userID!: number;
+    allUsers?: User[];
     user?: User;
-    updatedName?: string;
-    updatedPassword?: string;
-    updatedRole?: string;
+    errorMessage: string = '';
+    updateUserForm = this.fb.group({
+        updatedName: '',
+        updatedPassword: '',
+        updatedRole: '',
+    });
 
     constructor(
+        private fb: FormBuilder,
         public activeModal: NgbActiveModal,
         private userService: UserService
     ) { }
 
     ngOnInit(): void {
+        this.userService.getAllUsers().subscribe({
+            next: data => this.allUsers = data,
+            error: err => console.error("ERROR - Could not retrieve all users"),
+            complete: () => console.log("SUCCESS - All users retrieved")
+        });
+
         this.userService.getUserToModify(this.userID).subscribe({
             next: user => this.user = user,
             error: err => console.error("ERROR - Could not retrieve user"),
@@ -29,13 +41,42 @@ export class UpdateComponent implements OnInit {
         });
     }
 
+    get updatedNameControl(): FormControl {
+        return this.updateUserForm.get('updatedName') as FormControl;
+    }
+    
+    get updatedPasswordControl(): FormControl {
+        return this.updateUserForm.get('updatedPassword') as FormControl;
+    }
+    
+    get updatedRoleControl(): FormControl {
+        return this.updateUserForm.get('updatedRole') as FormControl;
+    }
+
     updateUser() {
         let updatedUser: User = <User> {
             ...this.user,
-            username: this.updatedName ? this.updatedName : this.user?.username,
-            password: this.updatedPassword ? this.updatedPassword : this.user?.password,
-            role: this.updatedRole ? this.updatedRole : this.user?.role
+            username: this.updatedNameControl.value ? this.updatedNameControl.value : this.user?.username,
+            password: this.updatedPasswordControl.value ? this.updatedPasswordControl.value : this.user?.password,
+            role: this.updatedRoleControl.value ? this.updatedRoleControl.value : this.user?.role
         };
-        this.activeModal.close(updatedUser);
+
+        if (this.accountExists(updatedUser.username, updatedUser.password)) {
+            this.errorMessage = "This username and password already exists!";
+            this.updateUserForm.reset();
+        }
+        else {
+            this.activeModal.close(updatedUser);
+        }
+    }
+
+    accountExists(uname?: string, pwd?: string) {
+        let exists = false;
+        this.allUsers?.forEach(user => {
+            if (uname === user.username && pwd === user.password) {
+                exists = true;
+            }
+        });
+        return exists;
     }
 }
