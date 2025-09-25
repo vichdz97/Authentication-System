0e177ce76fbe8c3a978468a56e32cef29f39a457
Victor Hernandez, Jr <vh4697@gmail.com>
2025-09-25 15:23:05 -0500

    updated Angular v15 -> v16

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router, UrlTree } from '@angular/router';
+import { Router, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 
 @Injectable({
     providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
     constructor(
         private userService: UserService,
