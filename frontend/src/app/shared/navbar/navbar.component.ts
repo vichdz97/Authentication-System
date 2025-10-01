027bd9ebb070cb914ab14b58b65f3618459e7865
Victor Hernandez, Jr <vh4697@gmail.com>
2025-09-30 23:12:45 -0500

    reset currentUser when logging out

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
     selector: 'app-navbar',
@@ -11,6 +14,14 @@ export class NavbarComponent {
     @Input() user: any;
     isCollapsed: boolean = true;
 
-    constructor() { }
+    constructor(
+        private userService: UserService,
+        private router: Router
+    ) { }
+
+    logout() {
+        this.userService.currentUser = <User>{};
+        this.router.navigateByUrl('/login');
+    }
 
 }
